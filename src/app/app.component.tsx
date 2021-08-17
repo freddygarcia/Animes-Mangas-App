54ac94b5e75e050539aedc1c6872bca327aa649e
Freddy Garcia <freddie-wpy@outlook.es>
2021-08-17 01:50:15 -0400

    Add Kitten provider to display Kitten components

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import * as eva from '@eva-design/eva';
 import { AppNavigator } from '../navigation/app.navigation';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 const App = () => {
     return (
-        <AppNavigator />
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <AppNavigator />
+        </ApplicationProvider>
     );
 };
 
