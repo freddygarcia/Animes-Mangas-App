5e9df3b25ce17c3f1b9900d894529ebaed4e171b
Freddy Garcia <freddie-wpy@outlook.es>
2021-08-22 04:19:43 -0400

    Share series title to other applications

--- src/components/navigation/TopNavigationBackward.tsx
+++ src/components/navigation/TopNavigationBackward.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Icon, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { Share } from 'react-native';
+import { RouteProp } from '@react-navigation/native';
+import { Manga } from '../../models/manga.model';
+import { Anime } from '../../models/anime.model';
 
 interface TopNavigationBackwardProps {
     title: string
     navigation: NativeStackNavigationProp<{}>;
+    route: RouteProp<{ params: { item: Anime | Manga } }>;
 };
 
 const TopNavigationBackward = (props: TopNavigationBackwardProps) => {
+
+    const shareTitle = async () => {
+        const message = props.route.params.item.title;
+        Share.share({ message })
+    }
 
     const renderMenuAction = (): React.ReactElement => (
         <TopNavigationAction
@@ -16,10 +26,18 @@ const TopNavigationBackward = (props: TopNavigationBackwardProps) => {
         />
     );
 
+    const renderShareAction = (): React.ReactElement => (
+        <TopNavigationAction
+            icon={<Icon name='share' />}
+            onPress={shareTitle}
+        />
+    )
+
     return (
         <TopNavigation
             title={props.title}
             accessoryLeft={renderMenuAction}
+            accessoryRight={renderShareAction}
         />
     )
 }
