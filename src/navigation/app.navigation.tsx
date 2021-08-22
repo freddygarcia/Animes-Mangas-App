a20d4cbfb7380916ea9202f284d56c7cd771e974
Freddy Garcia <freddie-wpy@outlook.es>
2021-08-22 14:46:57 -0400

    Add Splash Screen

--- src/navigation/app.navigation.tsx
+++ src/navigation/app.navigation.tsx
@@ -3,11 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AnimesNavigation from './anime.navigation';
 import MangasNavigation from './manga.navigation';
+import RNBootSplash from "react-native-bootsplash";
 
 const Drawer = createDrawerNavigator();
 
 export const AppNavigator = (): React.ReactElement => (
-    <NavigationContainer>
+    <NavigationContainer onReady={RNBootSplash.hide}>
         <Drawer.Navigator screenOptions={{ headerShown: false }}>
             {/* <Drawer.Screen name="Home" component={HomeScreen} /> */}
             <Drawer.Screen name="Animes" component={AnimesNavigation} />
